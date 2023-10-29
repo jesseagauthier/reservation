33b46c2b589549638a167dae9a1102f1b874c31e
gaut0486 <gaut0486@algonquinlive.com>
2023-10-29 19:28:27 -0400

    prevent more than one table being selected

--- scripts/script.js
+++ scripts/script.js
@@ -61,167 +61,136 @@ let tablesList = [
 		reservations: [],
 	},
 ]
-// settings
-const timeOpen = 11
-const timeClosed = 18
-let time = timeOpen + `:00`
+const openingTime = 11
+const closingTime = 18
+let currentTime = openingTime + ':00'
+let count = 0
 
-const storedTablesList = JSON.parse(localStorage.getItem('tablesList'))
+// Load tables from local storage or create an empty list
+let tableList = JSON.parse(localStorage.getItem('tablesList')) || tablesList
 
-// Check if there's a stored list of tables and render accordingly
-if (storedTablesList) {
-	tablesList = storedTablesList
-	rendersReservationHours(timeOpen, timeClosed, tablesList)
-	renderTables(tablesList)
-}
-if (!storedTablesList) {
-	rendersReservationHours(timeOpen, timeClosed, tablesList)
-	renderTables(tablesList)
-}
-
-// This function generates a list of hours the restaurant is open.
-// It also initializes the reservations for each table if there are no stored reservations.
-function generatesOpenHours(timeOpen, timeClosed, tablesList) {
-	if (timeOpen >= timeClosed) {
+// Function to generate open hours
+function generateOpenHours(openingTime, closingTime, tableList) {
+	if (openingTime >= closingTime) {
 		throw new Error('Opening time must be less than closing time.')
 	}
-
-	const storedTablesList = JSON.parse(localStorage.getItem('tablesList'))
-
 	let hoursOpen = []
-	let count = timeOpen
-
-	while (count < timeClosed) {
-		hoursOpen.push(count + `:00`)
-
-		if (!storedTablesList && tablesList) {
-			for (let table of tablesList) {
-				if (!Array.isArray(table.reservations)) {
-					table.reservations = []
-				}
-				table.reservations.push({ time: count + `:00`, reserved: false })
+	for (let count = openingTime; count < closingTime; count++) {
+		hoursOpen.push(count + ':00')
+		hoursOpen.push(count + ':30')
+		for (let table of tableList) {
+			if (!Array.isArray(table.reservations)) {
+				table.reservations = []
 			}
+			table.reservations.push({ time: count + ':00', reserved: false })
+			table.reservations.push({ time: count + ':30', reserved: false })
 		}
-
-		if (count !== timeClosed - 1) {
-			hoursOpen.push(count + `:30`)
-
-			if (!storedTablesList && tablesList) {
-				for (let table of tablesList) {
-					if (!Array.isArray(table.reservations)) {
-						table.reservations = []
-					}
-					table.reservations.push({ time: count + `:30`, reserved: false })
-				}
-			}
-		}
-
-		count++
 	}
-
 	return hoursOpen
 }
 
-// This function renders the dropdown options for reservation hours based on the restaurant's open hours.
-function rendersReservationHours(timeOpen, timeClosed, tablesList) {
-	const openHours = generatesOpenHours(timeOpen, timeClosed, tablesList)
+// Function to render reservation hours in dropdown
+function renderReservationHours(openingTime, closingTime, tableList) {
 	const hoursContainer = document.getElementById('time')
-	let count = -1
-	for (const hour of openHours) {
-		count++
+	const openHours = generateOpenHours(openingTime, closingTime, tableList)
+	openHours.forEach((hour, index) => {
 		hoursContainer.insertAdjacentHTML(
 			'beforeend',
-			`<option class="bg-white text-md text-black" data-hour="${count}">${hour}</option>
-        `
+			`<option class="bg-white text-md text-black" data-hour="${index}">${hour}</option>`
 		)
-	}
+	})
 }
-// Event listener to detect clicks on tables for booking.
+
+// Event listener for table booking
 document.getElementById('tables').addEventListener('click', function (event) {
-	if (event.target.className.includes('table')) {
-		const table = event.target.dataset.table
-		const restrict = restrictSelected()
-		if (!restrict) {
-			bookTable(table, event)
-		} else {
-			alert('One Table Per Booking')
-			return
-		}
+	const previouslySelected = document.querySelectorAll("[data-selected='true']")
+	const table = event.target.dataset.table
+
+	if (previouslySelected.length === 1) {
+		removeSelected(previouslySelected)
+		bookTable(table, event)
+	} else {
+		bookTable(table, event)
 	}
 })
 
-// Event listener for the time dropdown.
-// Updates the global time variable whenever a new time is selected.
+function removeSelected(previouslySelected) {
+	previouslySelected[0].classList.remove('bg-orange-400')
+	previouslySelected[0].dataset.selected = 'false'
+}
+
+// Event listener for time dropdown
 const timeElement = document.getElementById('time')
 timeElement.addEventListener('change', (event) => {
-	time = event.target.value
-	console.log(time)
+	currentTime = event.target.value
+	console.log(currentTime)
 })
 
-// This function renders the tables on the page.
-function renderTables(tablesList) {
+// Function to render tables
+function renderTables(tableList) {
 	const tablesContainer = document.getElementById('tables')
-	for (const table of tablesList) {
+	tableList.forEach((table) => {
 		tablesContainer.insertAdjacentHTML(
 			'beforeend',
 			`
-            <div class="table border p-4 rounded col-span-1 cursor-pointer shadow border-3 hover:bg-green-400 hover:border-gray-500" data-table="${table.table}">
-            <span class="table font-bold text-xl" data-table="${table.table}">Seats ${table.seats}</span>
-            <p class="table mt-2" data-table="${table.table}">Table: ${table.table}</p>
-        </div>
-        `
+      <div class="table border p-4 rounded col-span-1 cursor-pointer shadow border-3 hover:bg-green-400 hover:border-gray-500" data-table="${table.table}">
+        <span class="font-bold text-xl" data-table="${table.table}">Seats ${table.seats}</span>
+        <p class="mt-2" data-table="${table.table}">Table: ${table.table}</p>
+      </div>
+    `
 		)
-	}
+	})
 }
-
 // This function handles the booking of a table for a given time.
 // It toggles the table's visual representation and updates the reservation status.
 function bookTable(table, event) {
+	if (
+		event.target.parentElement.dataset.selected == 'true' ||
+		event.target.dataset.selected == 'true'
+	) {
+		event.target.classList.toggle('hover:bg-green-400')
+		event.target.classList.toggle('bg-orange-400')
+	}
+
 	if (event.target.tagName == 'span' || event.target.tagName == 'p') {
 		event.target.parentElement.classList.toggle('hover:bg-green-400')
 		event.target.parentElement.classList.toggle('bg-orange-400')
+		event.parentElement.dataset.selected = 'true'
 	}
 	if (event.target.tagName == 'DIV') {
 		event.target.classList.toggle('hover:bg-green-400')
 		event.target.classList.toggle('bg-orange-400')
+		event.target.dataset.selected = 'true'
 	}
 
 	const hoursContainer = document.getElementById('time')
 	const selectedTime = hoursContainer.value
 
-	const timesOpen = generatesOpenHours(timeOpen, timeClosed, tablesList)
+	const timesOpen = generateOpenHours(openingTime, closingTime, tableList)
 
 	const selectedTimeIndex = timesOpen.findIndex((time) => time === selectedTime)
 	const tableIndex = tablesList.findIndex(
 		(currentTable) => currentTable.table == table
 	)
-	console.log(tableIndex)
 	const twoHourBlock = 4
 	let currentIndex = selectedTimeIndex
 
 	for (let i = 0; i < twoHourBlock; i++) {
-		if (tablesList[tableIndex].reservations[currentIndex]) {
-			tablesList[tableIndex].reservations[currentIndex].reserved = true
+		if (tableList[tableIndex].reservations[currentIndex]) {
+			tableList[tableIndex].reservations[currentIndex].reserved = true
 			currentIndex++
 		}
 	}
-	localStorage.setItem('tablesList', JSON.stringify(tablesList))
+	localStorage.setItem('tablesList', JSON.stringify(tableList))
 }
 
-let count = 0
 function restrictSelected() {
 	count++
-	if (count > 1) {
-		// console.log(true)
-		return true
-	}
-	if (count <= 1) {
-		// console.log(false)
-		return false
-	} else {
-		console.error(count)
-	}
+	return count > 1
 }
+
+renderReservationHours(openingTime, closingTime, tableList)
+renderTables(tableList)
 
 // Todo:
 // Prevent the user from adding more than one table per booking
